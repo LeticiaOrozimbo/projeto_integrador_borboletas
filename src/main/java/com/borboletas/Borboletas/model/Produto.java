3505cb00e6d58c3f79104bb52eac7ed38d35b44c
Letícia <leticia-dospassos@hotmail.com>
2021-08-09 19:54:47 -0300

    atualização de branch

--- src/main/java/com/borboletas/Borboletas/model/Produto.java
+++ src/main/java/com/borboletas/Borboletas/model/Produto.java
@@ -1,6 +1,5 @@
 package com.borboletas.Borboletas.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
