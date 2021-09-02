2daf5edb02d369dd657477cf1f77bc5f255e09fa
llauros <lucaslauro@live.com>
2021-09-02 18:58:46 -0300

    [FEATURE Grupo04 Task: Cadastro-Usuario-Contato]

---
+++ src/app/isobre-nos/isobre-nos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-isobre-nos',
+  templateUrl: './isobre-nos.component.html',
+  styleUrls: ['./isobre-nos.component.css']
+})
+export class IsobreNosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/model/UsuarioDTO.ts
@@ -0,0 +1,9 @@
+export class UsuarioDTO{
+    public id: number
+    public nome: string
+    public senha: string
+    public token: string
+    public usuario: string
+    public tipo: string
+    public foto: string
+}
